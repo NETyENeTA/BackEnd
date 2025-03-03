0fd1acd3e71b2b87fc3061a51b879eff508777fe
Tihon <150705626+NETyENeTA@users.noreply.github.com>
2025-03-03 16:59:18 +0000

    Fixes

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import ForeignKey, String, BigInteger
 from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
-engine = create_async_engine(url="sqlite+aiosqlite", echo=True)
+engine = create_async_engine(url="sqlite+aiosqlite:///db.sqlite3", echo=True)
 async_session = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 
@@ -27,9 +27,9 @@ class Task(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(128))
     completed: Mapped[bool] = mapped_column(default=False)
-    userID: Mapped[int] = mapped_column(ForeignKey("users.id"), ondelete="CASCADE")
+    userID: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
 
 
 async def init_db():
     async with engine.begin() as connection:
-        await connection.run_sysns(Base.metadata.create_all)
+        await connection.run_sync(Base.metadata.create_all)
